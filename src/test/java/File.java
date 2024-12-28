bd968c69092db14911967f98fece9a84c712ede1
Ilyasova Kanykei <kanykei@MacBook-Air-Ilyasova.local>
2024-12-28 18:43:45 +0600

    edabit

--- src/test/java/File.java
+++ src/test/java/File.java
@@ -1,2 +1,19 @@
 public class File {
+    public static void main(String[] args) {
+        System.out.println(solutions(1, 0, -1));
+        System.out.println(solutions(1, 0, 0));
+        System.out.println(solutions(1, 0, 1));
+    }
+    public static int solutions(int a, int b, int c) {
+        int D = b * b - 4 * a * c;
+
+
+        if (D > 0) {
+            return 2;
+        } else if (D == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
